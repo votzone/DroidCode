11ec576754b553fc81327a34112c97f8ccf1242f
wq <wq@votzone.com>
2018-04-21 17:45:04 +0800

    update dialog

--- Dialog/app/src/main/java/FragmentDialog/MyFrageDialog.java
+++ Dialog/app/src/main/java/FragmentDialog/MyFrageDialog.java
@@ -1,7 +1,9 @@
 package FragmentDialog;
 
 import android.app.Dialog;
+//import android.app.DialogFragment;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -21,6 +23,11 @@ public class MyFrageDialog extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.inherit_dialog_layout,container, false);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
     }
 
     @Override
